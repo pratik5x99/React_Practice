f360c4aa6a4a9bd858f158476ab12392bfc6d082
unknown <pratik18priyam@gmail.com>
2025-03-11 19:31:09 +0530

    created some files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>hello pratik</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello Pratik from Vite </h1>
+  )
+}
+
+export default App
